44c0dea1a4088fd789444b5be37546eb7e2e3c9b
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2023-01-21 21:05:56 +0545

    User registration

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const { check, validationResult } = require("express-validator")
+const bcrypt = require("bcryptjs")
+
+const User = require("../models/User")
 
 const router = express.Router()
 
@@ -16,17 +19,55 @@ router.post(
 			"Please enter a password with 6 or more passwords"
 		).isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req)
 
-		if (errors.isEmpty()) {
-			res.status(200).json({
-				success: true,
-			})
-		} else {
-			res.status(400).json({
+		if (!errors.isEmpty())
+			return res.status(400).json({
 				success: false,
 				errors: errors.array(),
+			})
+
+		try {
+			const { name, email, password } = req.body
+
+			const createdUser = await User.findOne({ email })
+
+			if (createdUser)
+				return res.status(400).json({
+					success: false,
+					errors: [
+						{
+							value: email,
+							msg: "User already exists with the email",
+							param: "email",
+							location: "body",
+						},
+					],
+				})
+
+			const user = new User({ name, email, password })
+
+			const salt = await bcrypt.genSalt(10)
+
+			user.password = await bcrypt.hash(password, salt)
+
+			await user.save()
+
+			res.status(201).json({
+				success: true,
+				data: user,
+			})
+		} catch (err) {
+			console.error(err.message)
+
+			res.status(500).json({
+				success: false,
+				errors: [
+					{
+						message: "Something went wrong",
+					},
+				],
 			})
 		}
 	}
