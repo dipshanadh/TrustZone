1b22d9e016281a29765ff3f6c9b9086f6507c204
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2023-01-22 10:44:06 +0545

    Get auth user

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,33 @@
-const express = require('express')
+const express = require("express")
 
 const router = express.Router()
+
+const auth = require("../middlewares/auth")
+const User = require("../models/User")
 
 // @route   api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => {
-	res.send('Auth route')
+router.get("/", auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select("-password")
+
+		res.status(200).json({
+			success: true,
+			data: user,
+		})
+	} catch (err) {
+		console.error(err.message)
+
+		res.status(500).json({
+			success: false,
+			errors: [
+				{
+					msg: "Something went wrong",
+				},
+			],
+		})
+	}
 })
 
 module.exports = router
