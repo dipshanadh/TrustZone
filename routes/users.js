6e97ec5a45fc5caa16eb421d62af2f520a2800ad
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2023-01-22 16:28:51 +0545

    Re-route into "/companies"

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,9 @@ const { getUser } = require("../controllers/users")
 
 const router = express.Router()
 
+// Re-route into "/companies"
+router.use("/:id/company", require("./companies"))
+
 router.get("/:id", getUser)
 
 module.exports = router
