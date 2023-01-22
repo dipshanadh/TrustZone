a726b09411f67087aae6663b44304cf16e14f716
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2023-01-22 13:30:30 +0545

    Implement asyncHandler

--- controllers/users.js
+++ controllers/users.js
@@ -1,41 +1,33 @@
 const User = require("../models/User")
 
+// utils
+const asyncHandler = require("../utils/asyncHandler")
+
 // @desc    Get a user
 // @route   GET /api/user/:id
 // @access  Public
-const getUser = async (req, res) => {
+const getUser = asyncHandler(async (req, res) => {
 	const id = req.params.id
 
-	try {
-		const user = await User.findById(id)
+	const user = await User.findById(id)
 
-		if (!user)
-			return res.status(500).json({
-				success: false,
-				errors: [
-					{
-						value: id,
-						msg: "Could not find the user",
-						param: "id",
-						location: "params",
-					},
-				],
-			})
-
-		res.status(200).json({
-			success: true,
-			data: user,
-		})
-	} catch (err) {
-		res.status(500).json({
+	if (!user)
+		return res.status(500).json({
 			success: false,
 			errors: [
 				{
-					msg: "Something went wrong",
+					value: id,
+					msg: "Could not find the user",
+					param: "id",
+					location: "params",
 				},
 			],
 		})
-	}
-}
+
+	res.status(200).json({
+		success: true,
+		data: user,
+	})
+})
 
 module.exports = { getUser }
