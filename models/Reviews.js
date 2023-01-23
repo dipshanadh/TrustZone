fc9591b1080f556e80ce12f7e21d08ed0e9dd924
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2023-01-23 20:28:12 +0545

    Review model

--- models/Reviews.js
+++ models/Reviews.js
@@ -6,6 +6,40 @@ const ReviewSchema = new mongoose.Schema({
 		required: true,
 		maxlength: 50,
 	},
+	text: {
+		type: String,
+		required: true,
+		maxlength: 500,
+	},
+	rating: {
+		type: Number,
+		required: true,
+		min: 1,
+		max: 5,
+		default: 1,
+	},
+	likes: [
+		{
+			user: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "users",
+			},
+		},
+	],
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "users",
+		required: true,
+	},
+	company: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "companies",
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
 })
 
-module.exports = mongoose.model(ReviewSchema)
+module.exports = mongoose.model("Review", ReviewSchema)
