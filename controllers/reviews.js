96bc6a8acd3d0c960c0a1cfdd08c4f0643d02174
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2023-01-26 19:00:12 +0545

    Create revies

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -22,4 +22,42 @@ const getReviews = asyncHandler(async (req, res) => {
 	sendResponse(res, true, 200, reviews)
 })
 
-module.exports = { getReviews }
+// @route   POST api/companies/:id/reviews
+// @desc    Create a review
+// @access  Private
+const createReview = asyncHandler(async (req, res) => {
+	if (checkValidationErrors(req, res)) return
+
+	const companyID = req.params.id
+
+	const company = await Company.findOne({ user: req.user.id })
+
+	if (!company)
+		return sendResponse(res, false, 404, [
+			{ msg: "Could not find the company" },
+		])
+
+	const createdReview = await Reviews.findOne({
+		user: req.user.id,
+		company: companyID,
+	})
+
+	if (createdReview)
+		return sendResponse(res, false, 400, [
+			{ msg: "A user can't create more than one review for a company" },
+		])
+
+	const { title, text, rating } = req.body
+
+	const review = await Reviews.create({
+		title,
+		text,
+		rating,
+		company: companyID,
+		user: req.user.id,
+	})
+
+	sendResponse(res, true, 201, review)
+})
+
+module.exports = { getReviews, createReview }
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,34 @@
 const express = require("express")
 
+// middlewares
+const auth = require("../middlewares/auth")
+
 // controllers
-const { getReviews } = require("../controllers/reviews")
+const { getReviews, createReview } = require("../controllers/reviews")
+const { check } = require("express-validator")
 
 const router = express.Router({ mergeParams: true })
 
-router.route("/").get(getReviews)
+router
+	.route("/")
+	.get(getReviews)
+	.post(
+		[
+			auth,
+			check(
+				"title",
+				"Review title can not be more than 50 characters",
+			).isLength({ max: 50 }),
+			check(
+				"description",
+				"Company description can not be more than 500 characters",
+			).isLength({ max: 500 }),
+			check("rating")
+				.optional()
+				.isFloat({ min: 0, max: 5 })
+				.withMessage("Rating must be a number between 0 and 5"),
+		],
+		createReview,
+	)
 
 module.exports = router
